4d622bcbab327d312b5ed86fcec7b841df28500f
Trugamr <trugamr@live.in>
2021-12-06 22:37:54 +0530

    added umami analytics script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        async
+        defer
+        data-website-id="175206b1-64ae-4da5-9a2e-0ff7945bfe27"
+        src="https://umami.trugamr.dev/umami.js"
+      ></Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
