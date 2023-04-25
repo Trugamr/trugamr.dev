55e6fb0935aa3acb29c706f412d72467ad7ce5fd
Trugamr <trugamr@live.in>
2023-04-25 10:42:02 +0530

    fix: update tracking script url

--- app/root.tsx
+++ app/root.tsx
@@ -48,7 +48,7 @@ export const scripts: ExternalScriptsFunction = () => {
       defer: true,
       'data-website-id': '175206b1-64ae-4da5-9a2e-0ff7945bfe27',
       'data-domains': 'trugamr.dev',
-      src: 'https://umami.trugamr.dev/umami.js',
+      src: 'https://umami.trugamr.dev/script.js',
     },
   ]
 }
